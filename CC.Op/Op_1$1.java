5d65869d275c5c258f062434dd226389dd90b4d1
Conscience-Constellator <66545800+Conscience-Constellator@users.noreply.github.com>
2025-10-29 21:23:32 -0400

    Add files via upload

---
+++ CC.Op/Op_1$1.java
@@ -0,0 +1,18 @@
+package CC.Util;
+
+//import CC.Encycloped.Abs.Org.Comput.Soft.Ling.Java.Rap.Clas_Rap;
+//import static CC.Encycloped.Abs.Org.Comput.Soft.Ling.Java.Stat_Notif.*;
+
+import CC.$.Q$Q_Grup.Prim_A$A;
+
+public
+	interface Op_1$1
+	extends Prim_A$A
+{
+	/*
+	public static final Clas_Rap Class=Init_StRt(Op_2$1.class,
+		Clas_Rap.class);
+	/*Dep ?done*/
+
+//	static{Init_Nd(Op_2$1.class);}
+}
---
+++ CC.Op/Op_2$1.java
@@ -0,0 +1,55 @@
+package CC.Util;
+
+//import CC.Encycloped.Abs.Org.Comput.Soft.Ling.Java.Rap.Clas_Rap;
+//import static CC.Encycloped.Abs.Org.Comput.Soft.Ling.Java.Stat_Notif.*;
+import CC.COd.Finishd;
+import CC.COd.Lin_DclAr;
+import CC.COd.Neds_Ovrid;
+import static CC.COd.Neds_Ovrid.*;
+
+import CC.$.Q2$1_Grup.Prim_A2$1;
+
+public
+	interface Op_2$1
+	extends Prim_A2$1
+{
+	/*
+	public static final Clas_Rap Class=Init_StRt(Op_2$1.class,
+		Clas_Rap.class);
+	/*Dep ?done*/
+
+	@Lin_DclAr @Neds_Ovrid(NEds=No) @Finishd(Is_Finishd=true)
+	default void Op(boolean[] A,boolean[] B,boolean[] Rsult){Bool2$1(A,B,Rsult);}
+		@Lin_DclAr @Neds_Ovrid(NEds=No) @Finishd(Is_Finishd=true)
+		default boolean[] Op(boolean[] A,boolean[] B){return Bool2$1(A,B);}
+	@Lin_DclAr @Neds_Ovrid(NEds=No) @Finishd(Is_Finishd=true)
+	default void Op(byte[] A,byte[] B,byte[] Rsult){Byt2$1(A,B,Rsult);}
+		@Lin_DclAr @Neds_Ovrid(NEds=No) @Finishd(Is_Finishd=true)
+		default byte[] Op(byte[] A,byte[] B){return Byt2$1(A,B);}
+	@Lin_DclAr @Neds_Ovrid(NEds=No) @Finishd(Is_Finishd=true)
+	default void Op(char[] A,char[] B,char[] Rsult){ChR2$1(A,B,Rsult);}
+		@Lin_DclAr @Neds_Ovrid(NEds=No) @Finishd(Is_Finishd=true)
+		default char[] Op(char[] A,char[] B){return ChR2$1(A,B);}
+	@Lin_DclAr @Neds_Ovrid(NEds=No) @Finishd(Is_Finishd=true)
+	default void Op(short[] A,short[] B,short[] Rsult){Short2$1(A,B,Rsult);}
+		@Lin_DclAr @Neds_Ovrid(NEds=No) @Finishd(Is_Finishd=true)
+		default short[] Op(short[] A,short[] B){return Short2$1(A,B);}
+	@Lin_DclAr @Neds_Ovrid(NEds=No) @Finishd(Is_Finishd=true)
+	default void Op(float[] A,float[] B,float[] Rsult){FlOt2$1(A,B,Rsult);}
+		@Lin_DclAr @Neds_Ovrid(NEds=No) @Finishd(Is_Finishd=true)
+		default float[] Op(float[] A,float[] B){return FlOt2$1(A,B);}
+	@Lin_DclAr @Neds_Ovrid(NEds=No) @Finishd(Is_Finishd=true)
+	default void Op(int[] A,int[] B,int[] Rsult){Int2$1(A,B,Rsult);}
+		@Lin_DclAr @Neds_Ovrid(NEds=No) @Finishd(Is_Finishd=true)
+		default int[] Op(int[] A,int[] B){return Int2$1(A,B);}
+	@Lin_DclAr @Neds_Ovrid(NEds=No) @Finishd(Is_Finishd=true)
+	default void Op(double[] A,double[] B,double[] Rsult){Dubl2$1(A,B,Rsult);}
+		@Lin_DclAr @Neds_Ovrid(NEds=No) @Finishd(Is_Finishd=true)
+		default double[] Op(double[] A,double[] B){return Dubl2$1(A,B);}
+	@Lin_DclAr @Neds_Ovrid(NEds=No) @Finishd(Is_Finishd=true)
+	default void Op(long[] A,long[] B,long[] Rsult){Long2$1(A,B,Rsult);}
+		@Lin_DclAr @Neds_Ovrid(NEds=No) @Finishd(Is_Finishd=true)
+		default long[] Op(long[] A,long[] B){return Long2$1(A,B);}
+
+//	static{Init_Nd(Op_2$1.class);}
+}
